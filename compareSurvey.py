e7d34fe87f34d52204ea44e6bf6aa41df2c442d7
perhansson <per.hansson80@gmail.com>
2015-06-23 18:48:45 -0700

    add L4-6 u-channel survey

--- compareSurvey.py
+++ compareSurvey.py
@@ -1,5 +1,7 @@
-import sys,string, re
+#!/usr/bin/python
 
+import sys,string, re, subprocess
+from ROOT import TCanvas, TGraph
 
 
 
@@ -7,7 +9,7 @@ import sys,string, re
 def getVal(sv,i):
     return float(string.replace(sv.split()[i],',',''))
 
-def printcompare(ideal,surv,name):
+def printCompare(ideal,surv,name):
     f = open(name+'.txt','w')
     for k in sorted(ideal):
         if k in surv:
@@ -18,57 +20,111 @@ def printcompare(ideal,surv,name):
             f.write(s+'\n')
     f.close()
 
+def plotCompare(ideal,surv,name):
+    grD = [ TGraph() , TGraph(), TGraph()]
+    binLabels = {}
+    ix = 0
+    for k in sorted(ideal):
+        if k in surv:
+            v = ideal[k]
+            vs = surv[k]
+            delta = [ getVal(v,0)- getVal(vs,0), getVal(v,1)- getVal(vs,1), getVal(v,2)- getVal(vs,2) ]            
+            for i in range(len(grD)):
+                np = grD[i].GetN()
+                grD[i].SetPoint(np,ix,delta[i])
+            binLabels[ix] = k
+            ix = ix + 1
+    print binLabels
+    c = TCanvas('c','c',10,10,1200,700)
+    c.Print(name + '.ps[')
+    for i in range(len(grD)):
+        c1 = TCanvas('c'+str(i),'c'+str(i),10,10,1200,700)
+        c1.SetBottomMargin(0.25)
+        c1.SetGridy()
+        grD[i].SetTitle(name + ' survey;;' + ' Ideal-Surveyed in ' + ['x','y','z'][i] + ' (mm)')
+        grD[i].SetMarkerSize(1.0)
+        grD[i].SetMarkerStyle(20)
+        h = grD[i].GetHistogram()
+        for k,v in binLabels.iteritems():
+            b = h.FindBin(k)
+            h.GetXaxis().SetBinLabel(b, v)
+        grD[i].Draw('ALP')
+        #cans.append(c)
+        c1.Print(name + '.ps')
+    c.Print(name + '.ps]')
+    status = subprocess.call('ps2pdf ' + name + '.ps',shell=True)
+    ans = raw_input('continue?')
 
 
 
-def getSensorFramePos(f):
+
+
+def getSensorFramePos(lines):
 
     res = {}
-    for line in f.readlines():
+    for line in lines:
         if ' in ' not in line or 'sensor' in line or 'in mod' not in line or 'mm' not in line:
             continue
         m = re.search('module_L([1-6])([tb])_halfmodule_(\S+)',line)
         if m!=None:
+            print line
             res[ m.group(1)+m.group(2)+'_'+m.group(3) ] = line.split('[')[1].split(']')[0]
     return  res
 
 
-def getPinFramePos(f):
-    #print 'getpinfr ', len(f.readlines())
+def getPinFramePos(lines):
     res = {}
-    #print 'getpinfr ', len(f.readlines())    
-    for line in f.readlines():
-        #print line
-        #continue
-        #if ' in ' not in line or 'sensor' in line or 'in mod' not in line or 'mm' not in line:
-        #    continue
+    for line in lines:
         m = re.search('module_L([1-6])([tb]) (.*) base.* \[(.*)\] ',line)
         if m !=None:
-            #print m.group(1), ' ', m.group(4)
             res[ m.group(1)+m.group(2) ] = m.group(4)
+    return  res
+
+def getUChannelPos(lines):
+    res = {}
+    for line in lines:
+        #print line
+        m = re.search('^support_([a-z]+)_(L\d+)\s.*base.*\[(.*)\].*mm.*',line)
+        if m !=None:
+            res[ 'U-ch ' + m.group(1)+m.group(2) ] = m.group(3)
     return  res
 
 
 
 fs = open(sys.argv[1],'r')
+fs_lines = fs.readlines()
 fd = open(sys.argv[2],'r')
+fd_lines = fd.readlines()
+linesSurvey = list(fs_lines)
+linesIdeal = list(fd_lines)
 
-print fs
 
-sensorFrameSurveyed = getSensorFramePos(fs)    
-sensorFrameIdeal = getSensorFramePos(fd)    
 
-#pinFrameSurveyed = getPinFramePos(fs)    
-#pinFrameIdeal = getPinFramePos(fd)    
 
-#print pinFrameSurveyed
+sensorSurveyed = getSensorPos(linesSurvey)    
+sensorIdeal = getSensorPos(linesIdeal)    
+
+pinSurveyed = getPinPos(linesSurvey)    
+pinIdeal = getPinPos(linesIdeal)    
+
+
+channelIdeal = getUChannelPos(linesIdeal)    
+channelSurveyed = getUChannelPos(linesSurvey)    
 
 #print 'hej'
 #print pinFrameIdeal
 
 
-#printcompare(pinFrameIdeal,pinFrameSurveyed,'pinFrameDiff')
-printcompare(sensorFrameIdeal,sensorFrameSurveyed,'sensorFrameDiff')
+
+printCompare(sensorIdeal,sensorSurveyed,'sensorDiff')
+plotCompare(sensorIdeal,sensorSurveyed,'sensorDiff')
+
+printCompare(pinIdeal,pinSurveyed,'pinDiff')
+plotCompare(pinIdeal,pinSurveyed,'pinDiff')
+
+printCompare(channelIdeal,channelSurveyed,'channelDiff')
+plotCompare(channelIdeal,channelSurveyed,'channelDiff')
+
 
 fs.close()
 fd.close()
